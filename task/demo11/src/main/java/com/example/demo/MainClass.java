d26cb71b4cde833c53c6d8e18eb1e84574fb0149
pskokare <99635388+pskokare@users.noreply.github.com>
2024-05-18 10:02:12 +0530

    Add files via upload

---
+++ task/demo11/src/main/java/com/example/demo/MainClass.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Your application logic here
+        System.out.println("Hello, world!");
+    }
+}
